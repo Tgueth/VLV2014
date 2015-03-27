d5b3a663df7895c6ebd00a2b3490397cdf0e2556
tgueth <tgueth@binarystartechnology.com>
2015-03-27 08:41:31 -0400

    Working on a class so simplify the ul/li structure so easier to modify

--- VLV2014Test/Models/ViewModels.cs
+++ VLV2014Test/Models/ViewModels.cs
@@ -19,9 +19,19 @@ namespace VLV2014Test.Models
         public Sponsor Music { get; set; } 
     }
 
-    public class TabSectioinView
+    public class TabSectionView
     {
         public string pictureLink1 { get; set; }
         public string pictureLink2 { get; set; }
+        public string header { get; set; }
+        public string hLine1 { get; set; }
+        public string hLine2 { get; set;}
+        public string hLine3 { get; set; }
+        public string lLine1 { get; set; }
+        public string lLine2 { get; set; }
+        public string lLine3 { get; set; }
+        public string lLine4 { get; set; }
+        public string lLine5 { get; set; }
+        public string lLine6 { get; set; }
     }
 }
