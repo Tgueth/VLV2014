6a06ac4c042c52c675a7f8594df8a7189b3670ec
tgueth <tgueth@binarystartechnology.com>
2015-03-27 09:43:32 -0400

    Creating a partial view for use with this website template
    Created the partial view - ULsection
    
    ViewModel - created a class to hold the details
    
    Index View - playing with the setup

--- VLV2014Test/Models/ViewModels.cs
+++ VLV2014Test/Models/ViewModels.cs
@@ -33,5 +33,21 @@ namespace VLV2014Test.Models
         public string lLine4 { get; set; }
         public string lLine5 { get; set; }
         public string lLine6 { get; set; }
+
+        public TabSectionView(string str1, string str2, string str3, string str4, string str5, string str6, string str7, string str8, string str9, string str10, string str11, string str12)
+        {
+            pictureLink1 = str1;
+            pictureLink2 = str2;
+            header = str3;
+            hLine1 = str4;
+            hLine2 = str5;
+            hLine3 = str6;
+            lLine1 = str7;
+            lLine2 = str8;
+            lLine3 = str9;
+            lLine4 = str10;
+            lLine5 = str11;
+            lLine6 = str12;
+        }
     }
 }
